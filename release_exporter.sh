fba569f406442e5e6b5591395bee816c299234d3
lza_menace <lza_menace@protonmail.com>
2023-10-12 13:44:59 -0700

    remove extra script

--- release_exporter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t "lalanza808/monerod_exporter:1.0.0" -f monerod_exporter dockerfiles
-docker tag lalanza808/monerod_exporter:1.0.0 lalanza808/monerod_exporter:latest
-docker push lalanza808/monerod_exporter:1.0.0
-docker push lalanza808/monerod_exporter:latest
