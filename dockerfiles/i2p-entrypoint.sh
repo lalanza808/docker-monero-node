cd02a678494cf566d1fdd31445efc2fb01eb80e7
lalanza808 <lalanza808@users.noreply.github.com>
2024-12-22 14:51:13 -0800

    Update dockerfiles/i2p-entrypoint.sh
    Co-authored-by: nahuhh <50635951+nahuhh@users.noreply.github.com>

--- dockerfiles/i2p-entrypoint.sh
+++ dockerfiles/i2p-entrypoint.sh
@@ -4,6 +4,6 @@ chown -R i2p:i2p /home/i2p
 
 # Run i2pd
 sudo -u i2p i2pd \
-    --httpproxy.enabled 1 \
-    --httpproxy.address 0.0.0.0 \
-    --httpproxy.port 4444
+    --socksproxy.enabled 1 \
+    --socksproxy.address 0.0.0.0 \
+    --socksproxy.port 4447
