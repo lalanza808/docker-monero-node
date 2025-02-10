a630aeb9671f963a7f756ccf93c83a149d495850
Dvd-Znf <email@dvdznf.xyz>
2025-02-10 12:08:45 +0100

    Fix wrong service dir path in entrypoint script

--- dockerfiles/torentrypoint.sh
+++ dockerfiles/torentrypoint.sh
@@ -3,12 +3,12 @@
 tor &
 
 # Wait for onion address to be generated
-while [ ! -f /var/lib/tor/hidden_service/hostname ]; do
+while [ ! -f /var/lib/tor/monerod/hostname ]; do
   sleep 1
 done
 
 echo "=========================================="
-echo "Your Monero RPC Onion address is: $(cat /var/lib/tor/hidden_service/hostname)"
+echo "Your Monero RPC Onion address is: $(cat /var/lib/tor/monerod/hostname)"
 echo "=========================================="
 
 # Keep alive
